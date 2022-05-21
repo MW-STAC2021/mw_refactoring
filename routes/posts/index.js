d76c0b112a087288244854f6a778b0fbf0057073
Min J <m04j00@gmail.com>
2022-05-22 03:50:28 +0900

    feat : 즐겨찾기 추가 및 삭제, 목록 구현

--- routes/posts/index.js
+++ routes/posts/index.js
@@ -4,5 +4,7 @@ const postCtrl = require('./posts.ctrl');
 
 router.get('/', postCtrl.getPosts);
 router.get('/:id', postCtrl.getPost);
+router.post('/keep', postCtrl.keep);
+router.get('/keep/:userId', postCtrl.getkeeps);
 
 module.exports = router;
--- routes/posts/posts.ctrl.js
+++ routes/posts/posts.ctrl.js
@@ -1,4 +1,8 @@
 const sequelize = require('sequelize');
+const {
+    User,
+    Keep
+} = require('../../models');
 const models = require('../../models');
 const Op = sequelize.Op;
 
@@ -62,6 +66,108 @@ exports.getPost = async (req, res) => {
         console.log(err);
         return res.status(500).json({
             message: "서버 오류",
+        });
+    }
+}
+
+exports.keep = async (req, res) => {
+    const userId = req.body.userId;
+    const postId = req.body.postId;
+
+    try {
+        const existUser = await models.User.findOne({
+            where: {
+                id: userId
+            }
+        });
+        if (!existUser) {
+            return res.status(404).json({
+                message: "계정이 존재하지 않습니다.",
+            });
+        }
+        const existPost = await models.Post.findOne({
+            where: {
+                id: postId
+            }
+        });
+        if (!existPost) {
+            return res.status(404).json({
+                message: "글이 존재하지 않습니다.",
+            });
+        }
+        const existKeep = await models.Keep.findOne({
+            where: {
+                user_id: userId,
+                post_id: postId
+            }
+        });
+
+        if (existKeep) {
+            await models.Keep.destroy({
+                where: {
+                    user_id: userId,
+                    post_id: postId
+                },
+                force: true
+            });
+            return res.status(200).json({
+                message: "즐겨찾기에서 삭제했습니다.",
+            });
+        } else {
+            await models.Keep.create({
+                user_id: userId,
+                post_id: postId
+            });
+            return res.status(200).json({
+                message: "즐겨찾기에 추가했습니다.",
+            });
+        }
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            message: '서버 오류'
+        });
+    }
+}
+
+exports.getkeeps = async (req, res) => {
+    const userId = req.params.userId;
+    console.log("userId ", userId)
+    try {
+        const existUser = await models.User.findOne({
+            where: {
+                id: userId
+            }
+        });
+        if (!existUser) {
+            return res.status(404).json({
+                message: "계정이 존재하지 않습니다.",
+            });
+        }
+        const data = await models.Keep.findAll({
+            attributes: [],
+            where: {
+                user_id: userId,
+            },
+            include: [{
+                model: models.Post,
+                attributes: [
+                    'id',
+                    'category',
+                    'title',
+                    'thumbnail_path'
+                ],
+                required: true,
+
+            }]
+        });
+        return res.status(200).json({
+            data
+        });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            message: '서버 오류'
         });
     }
 }
